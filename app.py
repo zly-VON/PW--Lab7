acdb4732fc4e77f7c5d6233b0d8c5e2ad229baaa
zly-VON <milenacojuhari@gmail.com>
2024-05-23 22:00:44 +0300

    feat/Add swagger documentation

--- app.py
+++ app.py
@@ -16,7 +16,17 @@ if __name__ == '__main__':
     app = create_app()
     import routes
     app.config['JWT_SECRET_KEY'] = 'secret'
-    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(minutes=1)
+    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(minutes=60)
     jwt = JWTManager(app)
+
+    SWAGGER_URL = '/swagger'
+    API_URL = '/static/swagger.json'
+    swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={'App Name': 'Gym Workout API'}
+    )
+    app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+
     
     app.run(debug=True)
